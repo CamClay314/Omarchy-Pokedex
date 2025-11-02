6286b01f4a4b9db72d66fcedcef5c08590617fc9
Cameron Clay <camclay314@gmail.com>
2025-11-02 04:10:06 -0600

    LOVE MEEE

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import pokebase as pb
+
 
 app = Flask(__name__)
 
@@ -6,12 +8,22 @@ app = Flask(__name__)
 def home():
     return "Welcome to Omarchy Pokedex!"
 
+
+#Pokedex Route for url handling
 @app.route('/Pokedex')
 def Pokedex():
     response = "Welcome Trainer!<br>"
     response += "This is a placeholder route.<br>"
     response += "More features coming soon!"
     return response
+
+# Get the Pokemon
+def get_pokemon(pokemon_name):
+
+    pokemon = pb.pokemon(pokemon_name)
+    
+    return 
+
 
 
 if __name__ == '__main__':
